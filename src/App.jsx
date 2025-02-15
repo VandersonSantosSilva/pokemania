ef3b2770f7d71ad075fb28eab54b90b6b5c43584
Vanderson Santos <93881616+VandersonSantosSilva@users.noreply.github.com>
2025-02-15 12:28:52 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PokemonsReducers from './PokedexReducers/PokemonsReducers';
+import PokemonsList from './Components/PokemonsList';
+import Search from './Components/Search';
+import InformationsPokemons from './Components/InformationsPokemons';
+import { Routes, Route } from 'react-router-dom';
+
+const store = createStore(PokemonsReducers);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Routes>
+        <Route exact path="/" element={[ <Search />, <PokemonsList />]} />
+        <Route path={`/pokemon/:id`} element={<InformationsPokemons />} />
+      </Routes>
+    </Provider>
+  );
+}
+
+export default App;
